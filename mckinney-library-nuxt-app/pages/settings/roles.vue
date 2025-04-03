bc78919dc78804de2e8a7f7cccc12ab1c7e899fe
Arjun-Kaimal <akaimal04@gmail.com>
2025-04-03 15:49:52 -0500

    Revert "Merge branch 'view-roles-page' into development"
    This reverts commit b680cf57c93e162ff67a2b582602206cfc41147e, reversing
    changes made to 4ed1b1ff284579cd560056396ba3c14c495fb5a9.

--- mckinney-library-nuxt-app/pages/settings/roles.vue
+++ mckinney-library-nuxt-app/pages/settings/roles.vue
@@ -4,45 +4,40 @@
         <thead>
           <tr>
             <th> Role Name </th>
-            <th> View "Donations" and "Grants" </th>
-            <th> Edit "Donations" and "Grants" </th>
-            <th> View "Settings" </th>
-            <th> Edit "Manage Accounts" </th>
-            <th> Edit "Create Accounts" </th>
+            <th> View Donations/Grants </th>
+            <th> Edit Donations/Grants </th>
+            <th> View Settings </th>
+            <th> Edit Settings </th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td> Main Admin </td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
           </tr>
           <tr>
             <td> Admin </td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
           </tr>
           <tr>
             <td> Editor </td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:200%; font-weight:bold; color:#545679;" >&#215;</td>
           </tr>
           <tr>
             <td> Viewer </td>
-            <td style="text-align:center; font-size:150%; font-weight:bold; color:green;" >&#10004;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
-            <td style="text-align:center; font-size:200%; font-weight:bold; color:red;" >&#215;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:200%; font-weight:bold; color:#545679;" >&#215;</td>
+            <td style="text-align:center; font-size:150%; font-weight:bold; color:#545679;" >&#10004;</td>
+            <td style="text-align:center; font-size:200%; font-weight:bold; color:#545679;" >&#215;</td>
           </tr>
         </tbody>
       </table>
