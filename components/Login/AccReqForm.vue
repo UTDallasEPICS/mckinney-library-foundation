9e482f215603780fa0b182d254cfb663ce737717
JasonLaPorte <jasonlaporte04@gmail.com>
2025-11-04 16:03:27 -0600

    starting to add account creation

--- components/Login/AccReqForm.vue
+++ components/Login/AccReqForm.vue
@@ -27,7 +27,35 @@
     async function requestAccount(values:Record<string,any>){
         alert("account requested");
         console.log(values);
-        //send email with confirmation link to admin
+        //1. send email with confirmation link to admin -> 2. link brings up confirmation page -> 3. creates accounts 
+            //1. use nodemailer, generate security token 
+            //2. very simple, account info + button to accept or deny
+            //3. API for account creation works so long as id can be generated
+        // (account modifications should be handled in settings by admin/main admin)
     }
+
+/*ALT OPTION
+    make a page that shows account requests
+    just send admin an email that there was a request made by [info], still have to log on to approve?
+    think this is easier and more secure but we'll see
+
+    required files for both: APIs for request creation and retrival, API to create a user, page for showing request(s) restricted to op2? main admin access : token
+                             way to store outstanding requests in database
+*/
+    const {sendMail} = useNodeMailer();
+
+    try{
+        const info = sendMail({
+        subject: "test",
+        text: "a link eventually", // {siteURL}/page_for_confirmations?some_security_token_probably
+        to: "admin@email.com" // should be the same as sender for now
+        })
+        console.log("Email Sent: ", info);
+    }catch(error){
+        console.error("Error Sending Email: ", error);
+    }
+
+
+    
 
 </script>
