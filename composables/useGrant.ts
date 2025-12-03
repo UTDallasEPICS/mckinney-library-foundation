326733a19153d86ee45a86b2aa9f055e211201ae
diepndoan <ngocdiep.2k4@gmail.com>
2025-12-02 22:52:30 -0600

    add grant composables

---
+++ composables/useGrant.ts
@@ -0,0 +1,73 @@
+import { ref } from 'vue';
+import type { Grant } from '~/interfaces/grant'
+
+export function useGrants() {
+    const grants = ref<Grant[]>([]);
+    const selectedGrant = ref(null);
+
+    async function getGrants(){
+        try {
+            const route: string = `/api/grants`;
+            grants.value = await $fetch<Grant[]>(route);
+    
+        } catch (error) {
+            console.error('getGrants Error:', error);
+        }
+    };
+
+    async function getGrant(selectedGrant: Grant) {
+        try {
+            const route: string = `/api/grants/${selectedGrant.id}`;
+            selectedGrant = await $fetch<Grant>(route);
+        } catch (error) {
+            console.error('getGrant Error:', error);
+        }
+    }
+
+    async function updateGrant(selectedGrant: Grant) {
+        try {
+            await $fetch(`/api/grants/${selectedGrant.id}`, {
+                method: "PUT",
+                body: selectedGrant
+            });
+
+        } catch (error) {
+            console.error('updateGrant Error:', error);
+        }
+    }
+
+    async function createGrant(selectedGrant: Grant) {
+        try {
+            const route: string = `/api/grants`;
+            await $fetch(route, {
+                method: "POST",
+                body: selectedGrant
+            });
+
+        } catch (error) {
+            console.error('createGrant Error:', error);
+        }
+    }
+
+    async function deleteGrant(selectedGrant: Grant) {
+        try {
+            const route: string = `/api/grants/${selectedGrant.id}`;
+            await $fetch(route, {
+                method: "DELETE",
+            });
+
+        } catch (error) {
+            console.error('deleteGrant Error:', error);
+        }
+    }
+
+    return {
+        grants,
+        selectedGrant,
+        getGrants,
+        getGrant,
+        updateGrant,
+        createGrant,
+        deleteGrant
+    }
+}
