c83f2d43b26a513eee3eb3818cb3df1c4101d7c8
diepndoan <ngocdiep.2k4@gmail.com>
2025-12-02 22:53:19 -0600

    grant index page

--- pages/grants/index.vue
+++ pages/grants/index.vue
@@ -1,22 +1,32 @@
 <template>
-  <div>
-    <h1> grant table </h1>
-  </div>
+
+  <grantsBar :permission-level="permissionLevel"/>
+  <GrantsTable :grants = "grants" />
+  <GrantForm 
+      v-if = "showMenu"
+      @close = "showMenu = false"
+      @created = "getGrants()"
+    />
+
 </template>
 
 <script setup lang="ts">
-import { useAuth } from '~/composables/useAuth';
 
-const {session, getSession} = useAuth();
-session.value = await getSession();
+  import GrantsTable from '~/components/Tables/GrantsTable.vue';
+  import GrantForm from '~/components/Forms/GrantForm.vue';
+  import grantsBar from '~/components/grantsBar.vue';
+  import {onMounted,ref} from 'vue';  
+  import { useAuth } from '~/composables/useAuth';
 
-const permissionLevel = ref(0);
+  const {session, getSession} = useAuth();
+  session.value = await getSession();
+  const permissionLevel = ref(0);
+  if(session.value?.user){
+      permissionLevel.value = session.value.user.permission;
+  }
+  const { grants, getGrants} = useGrants();
+  const showMenu = ref(false);
 
-if(session.value?.user){
-  permissionLevel.value = session.value.user.permission;
-}
-else{
-  navigateTo("/");
-}
+  onMounted (() => getGrants())
 
 </script>
