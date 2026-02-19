604d56733123ecfbc7434681994b6245df6f1c9b
Jorge J Quintero <jjq200000@utdallas.edu>
2026-02-19 11:13:22 -0600

    Changing db imports and removing old lines

--- server/api/grant/index.post.ts
+++ server/api/grant/index.post.ts
@@ -1,7 +1,5 @@
-//index.post.ts grant folder
-import { PrismaClient } from '@prisma/client'
+import prisma from '~~/server/utils/prisma'
 
-const prisma = new PrismaClient()
 
 export default defineEventHandler(async (event) => {
     try {
