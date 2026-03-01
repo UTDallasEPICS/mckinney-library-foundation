2273a31be522644082268ca11191bbaabda27aae
Aaron Menezes <123612000+aaron1857@users.noreply.github.com>
2026-02-28 18:52:12 -0600

    clean up dockerfile, add prisma migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Apply migrations
+# Apply migrations and initialize migrations if it does not exist
 npx prisma generate
-npx prisma migrate dev
+npx prisma migrate deploy
+# Delete node_modules and prisma since migrations are done
+rm -rf node_modules
+
 # Run the CMD command from the dockerfile
 exec "$@"
