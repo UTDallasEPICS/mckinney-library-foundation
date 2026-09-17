30d578ba82614afe092460beb438b48c1fcb0dcc
Jaden Tran <Tranjad001@gmail.com>
2026-09-17 15:19:19 -0500

    chore: ignore prisma seed file

--- server/utils/seed.ts
+++ server/utils/seed.ts
@@ -7,6 +7,8 @@ const prisma = new PrismaClient({ adapter });
 
 const validUsers = [
   { name: "Jaden Tran", email: "tranjad001@gmail.com", permission: 3 },
+  { name: "Jack Matthews", email: "jxm230082@utdallas.edu", permission: 3},
+  { name: "", email: "", permission: 3},
 ];
 
 const donors = [
